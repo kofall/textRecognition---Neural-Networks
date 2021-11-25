b5b1f33a3c1ec255b2c0b76d4962f8f9c3c2d8c2
kofall <szymon.kowalski1707@gmail.com>
2021-11-25 17:56:17 +0100

    update kolejny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mosaic as msc
 from preProcess import preProcess
+from predict import predict as prd
 
 ########################################################################
 imgPath = 'data/sample04.png'
@@ -20,6 +21,8 @@ def projection():
         mosaic = msc.imgStacking([[pp.getImage(), pp.getGrayedImage(), pp.getCanniedImage(), pp.getContouredImage(),
                                    pp.getContouredImageWithBox()]] + pp.getCutImages(),
                                  (1500, 700))
+        x = prd(pp.getCutImages()[0][0])
+        print("Litera: {}\n".format(x))
         showImg(mosaic)
         pp.updateStatus()
         if cv2.waitKey(1) & 0xFF == ord('q'):
