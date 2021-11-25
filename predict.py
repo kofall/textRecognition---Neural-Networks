1589545d3fc6273f18d52876d155ab055861eb93
= <=>
2021-11-25 01:11:07 +0100

    Restructuring and adding predict.py

---
+++ predict.py
@@ -0,0 +1,102 @@
+import torchvision.transforms as T
+import torch
+import torch.nn as nn
+import numpy as np
+
+MODEL_PATH = "./models/model.pth"
+
+classes = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(64*64,2048),
+            nn.ReLU(),
+            nn.Linear(2048,2048),
+            nn.ReLU(),
+            nn.Linear(2048, 62)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def predict(array):
+    transform = T.Compose([
+        T.ToPILImage(),
+        T.Resize(64),
+        T.ToTensor()])
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.eval()
+    with torch.no_grad():
+        pred = model(transform(array))
+        predicted_letter = classes[pred.argmax(0)]
+        return predicted_letter
